e1cdd035dbbb41383077d191c661cb3bc852136a
Suchir Agrawal <suchiragrawal23@gmail.com>
2021-02-15 18:13:53 +0530

    http-module complete

--- http-example/index.js
+++ http-example/index.js
@@ -1,1 +1,52 @@
 const http = require('http')
+const hostname = "localhost"
+const port = 3000
+const fs = require('fs')
+const path= require('path')
+
+const server = http.createServer((req, res)=>{
+    console.log("Request for " +req.url+ " by method "+req.method);
+    if(req.method == "GET"){
+        var fileUrl;
+        if(req.url =='/') fileUrl="/index.html";
+        else fileUrl=req.url;
+
+        var filePath = path.resolve('./public'+fileUrl);
+        console.log("fpath: "+filePath);
+        const fileExt=path.extname(filePath);
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                console.log(exists)
+                if(!exists){
+                    res.statusCode=404
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                                ' not found</h1></body></html>');
+                    return;
+                }
+                res.statusCode=200
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res)
+                //console.log(fs.createReadStream(filePath))
+            });
+        }
+        else{
+            res.statusCode=404
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                                ' not a HTML file</h1></body></html>');
+        }
+    }
+    else{
+        res.statusCode=404
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                                ' not Supported</h1></body></html>');
+    }
+    
+})
+
+
+server.listen(port, hostname, ()=>{
+    console.log(`Server gets started at http://${hostname}:${port}`)
+})
